d4a7bfb94543d5c761dc5b9b88594c84e72f3455
Amit Rawat <rawat.amit158@gmail.com>
2023-12-30 12:45:03 +0530

    change

---
+++ src/main/java/com/Utilities_Backend/KStdUtilitiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Utilities_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KStdUtilitiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KStdUtilitiesBackendApplication.class, args);
+	}
+
+}
