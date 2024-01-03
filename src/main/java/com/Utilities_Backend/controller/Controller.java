f899ff212a94673d0652b0b8a041ee1c5e259b4b
Nehapanwar23 <118891825+Nehapanwar23@users.noreply.github.com>
2024-01-03 16:11:13 +0530

    change

--- src/main/java/com/Utilities_Backend/controller/Controller.java
+++ src/main/java/com/Utilities_Backend/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.Utilities_Backend.controller;
 
+import com.Utilities_Backend.repository.feederRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import com.Utilities_Backend.repository.receivingRepository;
 public class Controller {
 	 @Autowired
 	    private  receivingRepository receiveRepository;
-	 
+
+	@Autowired
+	private feederRepository feederRepo;
 	 
 }
