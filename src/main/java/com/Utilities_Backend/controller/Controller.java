755e4c3106116ee8b4bd143bb9a86dab06a53bc3
Nehapanwar23 <118891825+Nehapanwar23@users.noreply.github.com>
2024-01-08 11:05:14 +0530

    change

--- src/main/java/com/Utilities_Backend/controller/Controller.java
+++ src/main/java/com/Utilities_Backend/controller/Controller.java
@@ -1,11 +1,19 @@
 package com.Utilities_Backend.controller;
 
+import com.Utilities_Backend.entity.feeder;
+import com.Utilities_Backend.entity.receivingStation;
 import com.Utilities_Backend.repository.feederRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Utilities_Backend.repository.receivingRepository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
@@ -16,5 +24,94 @@ public class Controller {
 
 	@Autowired
 	private feederRepository feederRepo;
+
+	@PostMapping("/rstdata")
+	public ResponseEntity<?> saveUserRequest(@RequestBody receivingStation receiveST) {
+		try {
+//			String RSID = receivingRepository.getRSID();
+			String stationName = receiveST.getStationName();
+			String reason = receiveST.getReason();
+			String lastUpdate = receiveST.getLastUpdate();
+			String offTime = receiveST.getOffTime();
+			String onTime = receiveST.getOnTime();
+
+			boolean status = receiveST.isStatus();
+
+
+
+
+
+
+			// Create a new UserRequest entity
+			receivingStation newRST = new receivingStation();
+//			newRST.setFirstName(RSID);
+			newRST.setStationName(stationName);
+			newRST.setReason(reason);
+			newRST.setLastUpdate(lastUpdate);
+			newRST.setOffTime(offTime);
+			newRST.setOnTime(onTime);
+			newRST.setStatus(status);
+			newRST.setRSId(UUID.randomUUID());
+
+			// Save the UserRequest to the database
+			receivingStation savedUserRequest = receiveRepository.save(newRST);
+			return ResponseEntity.ok().body(savedUserRequest);
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Handle exceptions
+			return ResponseEntity.status(500).body("Internal Server Error");
+		}
+	}
+
+
+
+//	@PostMapping("/feeder")
+//	public ResponseEntity<?> saveFeeder(@RequestBody feeder Fd) {
+//		try {
+////            ObjectMapper objectMapper = new ObjectMapper();
+////            String json = objectMapper.writeValueAsString(consumer);
+////            System.out.println("Received request in saveLinkConsumer method. Request Body: " + json);
+//
+//			String FeederName = Fd.getFeederName();
+//			String Reason = Fd.getReason();
+//			String LastUpdate = Fd.getLastUpdate();
+//			String OffTime= Fd.getOffTime();
+//			String OnTime = Fd.getOnTime();
+//			UUID RSId = Fd.getReceivingStation().getRSId();
+//
+//			Optional<receivingStation> optionalreceivingStation = receivingRepository.findById(RSId);
+//			if (optionalreceivingStation.isPresent()) {
+//				receivingStation receivingStation = optionalreceivingStation.get();
+//
+//				feeder newFeeder = new feeder();
+//				newFeeder.setFeederName(FeederName);
+//				newFeeder.setReason(Reason);
+//				newFeeder.setLastUpdate(LastUpdate);
+//				newFeeder.setOffTime(OffTime);
+//				newFeeder.setOnTime(OnTime);
+//				newFeeder.setReceivingStation(receivingStation);
+//
+//				feeder savedFeeder = feederRepo.save(newFeeder);
+//
+//				return ResponseEntity.ok().body(savedFeeder);
+//			} else {
+//				return ResponseEntity.status(404).body("User not found with userId: " + userId);
+//			}
+//		} catch (Exception e) {
+//			if (isUniqueConstraintViolation(e)) {
+//				return ResponseEntity.status(409).body("Resource already exists. Please check your input.");
+//			} else {
+//				e.printStackTrace();
+//				return ResponseEntity.status(500).body("Internal Server Error");
+//			}
+//		}
+//	}
+//	private boolean isUniqueConstraintViolation(Exception e) {
+//		// Check if the exception is due to a unique constraint violation
+//		return e instanceof org.springframework.dao.DataIntegrityViolationException;
+//	}
+
+
+
 	 
 }
--- src/main/java/com/Utilities_Backend/repository/receivingRepository.java
+++ src/main/java/com/Utilities_Backend/repository/receivingRepository.java
@@ -1,5 +1,6 @@
 package com.Utilities_Backend.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,5 @@ import com.Utilities_Backend.entity.receivingStation;
 
 @Repository
 public interface  receivingRepository extends JpaRepository <receivingStation, UUID>{
-	
+    //Optional<receivingStation> findById(UUID RSId);
 }
